3825dc4f43d588be4dcb922a56f138bc97ae0c9e
Jeffrey Nelson <jdn5126@gmail.com>
2016-03-15 10:36:08 -0400

    static file structure

--- config.py
+++ config.py
@@ -7,5 +7,5 @@ ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'mp3', 'mp4
 SECRET_KEY = os.getenv("SECRET_KEY", "local-key")
 SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "discovery_rfid.db")
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-UPLOAD_FOLDER = '/tmp/'
+UPLOAD_FOLDER = basedir + '/app/static/media/'
 WTF_CSRF_ENABLED = True
